53bb78ea252ba525346681ca31a5ead3513d8568
thleima <thomas61.amiel@gmail.com>
2025-01-04 17:45:22 +0100

    check the code

--- todo_lists.js
+++ todo_lists.js
@@ -29,30 +29,23 @@ function displayTasks() {
 function createTaskComponent(task, index) {
   const taskComponent = document.createElement("div");
   taskComponent.classList.add("task-component");
-  taskComponent.innerHTML = `<input type="checkbox" id="task-${index}" ${
-    task.done ? "checked" : ""
-  }>
-        <label id="text-task-${index}" for="task-${index}">${task.text}</label>
-        `;
+  taskComponent.innerHTML = taskComponentHtml(task, index);
   taskComponent
     .querySelector("input")
-    .addEventListener("change", () => toggleTask(index));
+    .addEventListener("change", () => toggleTaskStatut(index));
   taskComponent
     .querySelector("label")
     .addEventListener("click", () => editTask(index));
   return taskComponent;
 }
 
-function toggleTask(index) {
+function toggleTaskStatut(index) {
   tasks[index].done = !tasks[index].done;
   console.log(tasks[index]);
-  // displayTasks();
 }
 
 function addInputForEdit(index) {
-  // get the element
   const thisTask = document.getElementById(`text-task-${index}`);
-  // add input to parent node to edit the task
   thisTask.parentNode.innerHTML += `<input type="text" id="edit-task-${index}" placeholder="Edit task"> <button id="btn-edit-${index}">Edit Task</button>`;
 }
 
@@ -69,4 +62,13 @@ function editTask(index) {
 function clearCompletedTasks() {
   tasks = tasks.filter((task) => !task.done);
   displayTasks();
+}
+
+function taskComponentHtml(task, index) {
+  const taskComponentHtml = `<input type="checkbox" id="task-${index}" ${
+    task.done ? "checked" : ""
+  }>
+        <label id="text-task-${index}" for="task-${index}">${task.text}</label>
+        `;
+  return taskComponentHtml;
 }
