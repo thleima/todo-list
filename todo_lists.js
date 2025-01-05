2f2f9bc3879d19c18e02b6f986a4b1d7d8647b5a
thleima <thomas61.amiel@gmail.com>
2025-01-05 11:28:02 +0100

    working on edit task

--- todo_lists.js
+++ todo_lists.js
@@ -15,7 +15,7 @@ function checkEmptyField(input) {
 function addTaskInArray() {
   const taskText = taskInput.value.trim();
   if (checkEmptyField(taskText)) {
-    tasks.push({ text: taskText, done: false });
+    tasks.push({ text: taskText, done: false, editing: false });
     taskInput.value = "";
     displayTasks();
   } else {
@@ -34,29 +34,38 @@ function taskComponent(task, index) {
   const taskComponent = document.createElement("div");
   taskComponent.classList.add("task-component");
   taskComponent.innerHTML = createTaskComponentHtml(task, index);
-  // Statut Done / Undone
-  taskComponent
-    .querySelector("input")
-    .addEventListener("change", () => toggleTaskStatut(index));
-  // Edit Task
-  taskComponent
-    .querySelector("label")
-    .addEventListener("click", () => editTask(index));
+  taskComponent.addEventListener("click", (e) => {
+    switch (e.target.nodeName) {
+      case "P":
+        toggleTaskStatut(index, "editing");
+        break;
+      case "INPUT":
+        toggleTaskStatut(index, "done");
+        break;
+      default:
+    }
+  });
   return taskComponent;
 }
 
-function toggleTaskStatut(index) {
-  tasks[index].done = !tasks[index].done;
+function toggleTaskStatut(index, statut) {
+  tasks[index][statut] = !tasks[index][statut];
+  console.log(tasks[index]);
+  return tasks[index][statut];
 }
 
 function editTask(index) {
-  addInputForEdit(index);
-  const editInput = document.getElementById(`edit-task-${index}`);
-  const editBtn = document.getElementById(`btn-edit-${index}`);
-  editBtn.addEventListener("click", () => {
-    tasks[index].text = editInput.value;
-    displayTasks();
-  });
+  if (toggleTaskStatut(index, "editing")) {
+    addInputForEdit(index);
+    const editInput = document.getElementById(`edit-task-${index}`);
+    const editBtn = document.getElementById(`btn-edit-${index}`);
+    editBtn.addEventListener("click", () => {
+      tasks[index].text = editInput.value;
+      displayTasks();
+    });
+  } else {
+    console.log("hello");
+  }
 }
 
 function addInputForEdit(index) {
@@ -73,7 +82,7 @@ function createTaskComponentHtml(task, index) {
   const taskComponentHtml = `<input type="checkbox" id="task-${index}" ${
     task.done ? "checked" : ""
   }>
-        <label id="text-task-${index}" for="task-${index}">${task.text}</label>
+        <p id="text-task-${index}">${task.text}</p>
         `;
   return taskComponentHtml;
 }
