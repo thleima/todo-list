94e22d2b02198b4c7b5820a5b0bf04c597c10cd7
thleima <thomas61.amiel@gmail.com>
2025-01-05 14:56:25 +0100

    all new structure on JS, create task and edit are working

---
+++ script.js
@@ -0,0 +1,81 @@
+const input = document.querySelector("#taskInput");
+const tasks_list = document.querySelector("#taskList");
+const add_task_btn = document.querySelector("#addTaskBtn");
+const clear_completed_btn = document.querySelector("#clearCompletedBtn");
+
+let tasks_array = [];
+
+add_task_btn.addEventListener("click", add_task_array);
+
+function add_task_array() {
+  const text = input.value.trim();
+  if (text !== "") {
+    tasks_array.push({ text: text, complete: false });
+    display_tasks();
+  } else {
+    alert("Empty Field");
+  }
+  input.value = "";
+}
+
+function display_tasks() {
+  tasks_list.innerHTML = "";
+  console.log("array :", tasks_array);
+  tasks_array.forEach((element, id) => {
+    create_task_component(element, id);
+  });
+}
+
+function create_task_component(task, id) {
+  const task_element = document.createElement("div");
+  task_element.classList.add("task-component");
+  task_element.setAttribute("id", `task-${id}`);
+
+  const task_content = document.createElement("div");
+  task_content.classList.add("content");
+
+  task_element.appendChild(task_content);
+
+  const task_input = document.createElement("input");
+  task_input.classList.add("text");
+  task_input.type = "text";
+  task_input.value = task.text;
+  task_input.setAttribute("readonly", "readonly");
+
+  task_content.appendChild(task_input);
+
+  const task_actions = document.createElement("div");
+  task_actions.classList.add("actions");
+
+  const task_edit_btn = document.createElement("button");
+  task_edit_btn.classList.add("edit");
+  task_edit_btn.innerText = "Edit";
+
+  const task_complete_btn = document.createElement("button");
+  task_complete_btn.classList.add("delete");
+  task_complete_btn.innerText = "Complete";
+
+  task_actions.appendChild(task_edit_btn);
+  task_actions.appendChild(task_complete_btn);
+
+  task_element.appendChild(task_actions);
+
+  tasks_list.appendChild(task_element);
+
+  task_edit_btn.addEventListener("click", (e) => {
+    if (task_edit_btn.innerText.toLowerCase() === "edit") {
+      task_edit_btn.innerText = "Save";
+      task_input.removeAttribute("readonly");
+      task_input.focus();
+    } else {
+      task_edit_btn.innerText = "Edit";
+      task_input.setAttribute("readonly", "readonly");
+      tasks_array[id].text = task_input.value;
+      display_tasks();
+    }
+  });
+
+  task_complete_btn.addEventListener("click", (e) => {
+    //tasks.list.removeChild(task_element);
+  });
+}
--- todo_lists.js
+++ todo_lists.js
@@ -34,44 +34,51 @@ function taskComponent(task, index) {
   const taskComponent = document.createElement("div");
   taskComponent.classList.add("task-component");
   taskComponent.innerHTML = createTaskComponentHtml(task, index);
+  const editBtn = taskComponent.querySelector(".edit");
+  const completeBtn = taskComponent.querySelector(".complete");
+  const taskText = taskComponent.querySelector(`#task-${index}`);
 
-  taskComponent.addEventListener("click", (e) => {
-    switch (e.target.className.toLowerCase()) {
-      case "edit":
-        console.log("edit");
-        break;
-      case "complete":
-        const actions = document.querySelector(".actions");
-        const taskText = document.querySelector(`#task-${index}`);
-        taskComponent.removeChild(actions);
-        taskText.classList.add("done");
-        toggleTaskStatut(index);
-        break;
-      default:
-    }
+  editBtn.addEventListener("click", (e) => {
+    console.log("edit");
   });
+
+  completeBtn.addEventListener("click", (e) => {
+    const actions = document.querySelector(".actions");
+    taskComponent.removeChild(actions);
+    taskText.classList.add("done");
+    toggleTaskStatut(index);
+  });
+
+  // taskComponent.addEventListener("click", (e) => {
+  //   switch (e.target.className.toLowerCase()) {
+  //     case "edit":
+  //       const editBtn = document.querySelector(".edit");
+  //       editBtn.innerText = "Save";
+  //       const taskText = document.querySelector(`#task-${index}`);
+  //       taskText.removeAttribute("readonly");
+  //       taskText.focus();
+  //       if ((editBtn.innerText = "Save")) {
+  //         editBtn.addEventListener("click", () => {
+  //           tasks[index].taskText = taskText.value;
+  //           taskText.setAttribute("readonly", "readonly");
+  //           editBtn.innerText = "Edit";
+  //           displayTasks();
+  //         });
+  //       }
+
+  //       break;
+  //     default:
+  //   }
+  // });
   return taskComponent;
 }
 
 function toggleTaskStatut(index) {
   tasks[index].done = !tasks[index].done;
+  console.log(tasks[index]);
 }
 
 function editTask(index) {}
-
-// function editTask(index) {
-//   if (toggleTaskStatut(index, "editing")) {
-//     addInputForEdit(index);
-//     const editInput = document.getElementById(`edit-task-${index}`);
-//     const editBtn = document.getElementById(`btn-edit-${index}`);
-//     editBtn.addEventListener("click", () => {
-//       tasks[index].text = editInput.value;
-//       displayTasks();
-//     });
-//   } else {
-//     console.log("hello");
-//   }
-// }
 
 function clearCompletedTasks() {
   tasks = tasks.filter((task) => !task.done);
@@ -84,14 +91,20 @@ function createTaskComponentHtml(task, index) {
                  <input 
                     id="task-${index}"
                     type="text" 
-                    class="text" 
+                    class="text ${task.done ? "done" : ""}" 
                     value="${task.text}"
                     readonly>
                 </div>
-                <div class="actions">
-                    <button class="edit">Edit</button>
-                    <button class="complete">Complete</button>
-                </div>
+                ${task.done ? actionsComponent() : ""}
+
             `;
   return taskComponentHtml;
+}
+
+function actionsComponent() {
+  const actionsComponent = `                <div class="actions">
+                    <button class="edit">Edit</button>
+                    <button class="complete">Complete</button>
+                </div>`;
+  return actionsComponent;
 }
